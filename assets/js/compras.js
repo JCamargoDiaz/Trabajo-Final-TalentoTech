252c76efc358e2ef4508a32412cfde557feef3b2
jocadi <135550170+JCamargoDiaz@users.noreply.github.com>
2024-12-06 23:55:25 -0500

    ajuste distribucion

--- assets/js/compras.js
+++ assets/js/compras.js
@@ -3,3 +3,26 @@
 document.getElementById('btn-pagar').addEventListener('click', function () {
   window.location.href = '/pago.html';
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  // variables
+  const baseDeDatos = [
+    {
+      "id": 1,
+      "nombre": "Nike Air Max",
+      "descripcion": "La mejor tecnología de Nike",
+      "precio": 5000,
+      "imagen": "./assets/img/productos/nike-8066-0927362-1",
+      "stock": 10
+    },
+    {
+      "id": 2,
+      "nombre": "Nike Air Max",
+      "descripcion": "La mejor tecnología de Nike",
+      "precio": 5000,
+      "imagen": "./assets/img/productos/nike-8905-0563552-1",
+      "stock": 10
+    },
+  ];
+});
